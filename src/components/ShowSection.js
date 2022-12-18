6aa75156d2318e28c4da59f8c93fcc489ae745cb
jnuwagaba <jpnuwgaba1@gmail.com>
2022-12-18 14:28:35 +0300

    mid-dec-2

--- src/components/ShowSection.js
+++ src/components/ShowSection.js
@@ -10,11 +10,11 @@ const ShowSection = ({ title, briefText }) => {
           <p>{briefText}</p>
         </div>
         <div className="show-section-grid">
-          <div className="show-section-grid-item-1"><ShowSectionGridItem showTitle={'Live in Kampala'} showDate={'03/01/2023'} img={'show1.jpg'} showLink={'Get a ticket'}/></div>
-          <div className="show-section-grid-item-2"><ShowSectionGridItem showTitle={'Live in Kampala'} showDate={'03/01/2023'} img={'show2.jpg'} showLink={'Catch up'}/></div>
-          <div className="show-section-grid-item-3"><ShowSectionGridItem showTitle={'Live in Kampala'} showDate={'03/01/2023'} img={'show3.jpg'} showLink={'Catch up'}/></div>
-          <div className="show-section-grid-item-4"><ShowSectionGridItem showTitle={'Live in Kampala'} showDate={'03/01/2023'} img={'show4.jpg'} showLink={'Catch up'}/></div>
-          <div className="show-section-grid-item-5"><ShowSectionGridItem showTitle={'Live in Kampala'} showDate={'03/01/2023'} img={'show5.jpg'} showLink={'Catch up'}/></div>
+          <div className="show-section-grid-item-1"><ShowSectionGridItem showTitle={'Show 1'} showDate={'03/01/2023'} img={'show1.jpg'} showLink={'Get a ticket'}/></div>
+          <div className="show-section-grid-item-2"><ShowSectionGridItem showTitle={'Show 2'} showDate={'03/01/2023'} img={'show2.jpg'} showLink={'Catch up'}/></div>
+          <div className="show-section-grid-item-3"><ShowSectionGridItem showTitle={'Show 3'} showDate={'03/01/2023'} img={'show3.jpg'} showLink={'Catch up'}/></div>
+          <div className="show-section-grid-item-4"><ShowSectionGridItem showTitle={'Show 4'} showDate={'03/01/2023'} img={'show4.jpg'} showLink={'Catch up'}/></div>
+          <div className="show-section-grid-item-5"><ShowSectionGridItem showTitle={'Show 5'} showDate={'03/01/2023'} img={'show5.jpg'} showLink={'Catch up'}/></div>
         </div>
       </div>
     </>
